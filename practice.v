c0c1ccab8f75368d8a57bc4fad031b0eb0961750
debra <debra@debby.myguest.virtualbox.org>
2023-05-08 22:10:04 -0400

    what eve

--- practice.v
+++ practice.v
@@ -1,1 +1,18 @@
-module
+module(
+	input wire a
+	input wire b
+	input wire sel
+	output reg z
+);
+
+	always_comb begin
+		if (sel == 1b'0) begin
+			z = a;
+		end;
+		else begin
+			z = b;
+		end
+	end
+
+endmodule;
+	 

2161cd571ffdd67c16e4520574536c05d60542db
debra <debra@debby.myguest.virtualbox.org>
2023-06-04 16:07:42 -0400

    updated practice

--- practice.v
+++ practice.v
@@ -1,23 +1,3 @@
-module m1(
-	input wire x;
-	output wire y;
-);
-
-assign x = y;
-
-endmodule
-
-module m2(
-	input wire a;
-	output wire b;
-);
-
-assign a = b;
-
-endmodule
-
-m1 mym1(.x(y);
-
-$display( //output of mym1)
+// Code a SIPO shift register, choose left, right or bi
 
 

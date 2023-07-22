0309372cb15497534a9e500ad89aeaf062efaa35
josh <joshua.eygenraam@ryerson.ca>
2023-07-22 14:50:10 -0400

    added systems eng q

---
+++ test_q/systems_eng_q.sv
@@ -0,0 +1,11 @@
+/*
+    Assume that you have a binary file that represents a block of memory from an embedded system. Write a function in Verilog to read this file and search for a specific sequence of bytes.
+
+    In firmware development, reading and writing to hardware registers is a common task. Write a function in Verilog to read and write to a memory-mapped I/O device.
+
+    Implement a simple power management unit in Verilog that can turn on and off components based on some criteria.
+
+    How would you write a testbench in Verilog for validating a new System-on-Chip (SoC) design?
+
+    Design a simple machine learning accelerator in Verilog. How would you handle communication with the host system and data transfer?
+*/

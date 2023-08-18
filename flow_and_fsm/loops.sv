aff6db2acf3787694d333be3373815fb83e4d9d9
root <joshua.eygenraam@torontomu.ca>
2023-08-19 04:24:38 +0800

    added gen vars

--- flow_and_fsm/loops.sv
+++ flow_and_fsm/loops.sv
@@ -12,3 +12,32 @@ inital begin
      #500 clk = ~clk;
   end
 end   
+*/
+
+// for loops
+  // fill-in later
+
+
+// gen var loops
+  // gen vars are synthesiazable, create multiple instances, modules, assigns, etc
+  // gets unrolled BEFORE simulation
+  // use when instantiating multiple hw objects
+  // use normal for time-simulation operations
+
+  generate
+      for (genvar i = 0; i < 4; i = i + 1) begin: gen_block
+          // Code to be replicated. For example, instances of a module.
+      end
+  endgenerate
+
+  // another example, gen 4 2to1 mux
+  module mux(input [3:0] a, b, output y);
+    generate
+      genvar i;
+      for (i=0; i<4; i=i+1) begin: gen_loop
+        assign y[i] = (sel) ? a[i] : b[i];
+      end
+    endgenerate
+  endmodule
+
+
